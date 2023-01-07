f59079ea8af3aa8b9ae4bb0082cffb4bb6c12f8b
Eliseeva <eliseevaulya@mail.ru>
2023-01-07 15:26:35 +0300

    add homework_04

---
+++ homework_04/main.py
@@ -0,0 +1,47 @@
+import asyncio
+from typing import List
+from models.base import Base
+from homework_04.jsonplaceholder_request import async_engine, get_users, get_posts
+from homework_04.models import User, Post
+
+
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def fetch_users() -> List[User]:
+    users = []
+    for user in await get_users():
+        user_ = User(id=user[id], name=user['name'], username=user['username'], email=user['mail'])
+        users.append(user_)
+    return users
+
+
+async def fetch_posts() -> List[Post]:
+    posts = []
+    for post in await get_posts():
+        post_ = Post(id=post['id'], user_id=post['userId'], title=post['title'], body=post['body'])
+        posts.append(post_)
+    return posts
+
+
+async def add_users():
+    users, posts = await asyncio.gather(
+        fetch_users(),
+        fetch_posts(),
+    )
+
+
+async def async_main():
+    await create_tables()
+    await add_users()
+
+
+def main():
+    asyncio.run(async_main())
+
+
+if __name__ == "__main__":
+    main()
